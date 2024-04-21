16236b186ef6d450aac7e284d84259d387b382ab
Gilad Nahor <giladn@hailo.ai>
2024-04-21 13:10:05 +0300

    added TAPPAS_LIBDIR to get_pkg_info.py

--- get_pkg_info.py
+++ get_pkg_info.py
@@ -3,7 +3,7 @@ import subprocess
 import os
 
 # This function gets the tappas_workspace and version to be used by the application
-# It first checks if the environment variables TAPPAS_WORKSPACE and TAPPAS_VERSION are set
+# It first checks if the environment variables TAPPAS_WORKSPACE, APPAS_LIBDIR and TAPPAS_VERSION are set
 # If they are not set, it uses pkg-config to get the data from the pkg-config file
 # If the pkg-config file is not accessible, it raises an exception
 # Make sure /opt/hailo/tappas/pkgconfig/hailo_tappas.pc is accessible and has the right owner and permissions
@@ -29,10 +29,20 @@ def get_pkg_info(package_name="hailo_tappas"):
             info['version'] = version
         else:
             info['version'] = TAPPAS_VERSION
+        # Get the libdir
+        TAPPAS_LIBDIR = os.environ.get("TAPPAS_LIBDIR", "")
+        if TAPPAS_LIBDIR == "":
+            libdir = subprocess.check_output(['pkg-config', '--variable=tappas_libdir', package_name], text=True).strip()
+            info['libdir'] = libdir
+        else:
+            info['libdir'] = TAPPAS_LIBDIR
 
     except subprocess.CalledProcessError:
         raise Exception("TAPPAS_WORKSPACE OR TAPPAS_VERSION environment variables are not set. Could not get data from pkg-config. Make sure /opt/hailo/tappas/pkgconfig/hailo_tappas.pc is accessible.")
-    print(f"Using TAPPAS_WORKSPACE: {info['tappas_workspace']} and TAPPAS version: {info['version']}")
+    print(f"Using TAPPAS_WORKSPACE: {info['tappas_workspace']}")
+    print(f"TAPPAS_LIBDIR: {info['libdir']}")
+    print(f"TAPPAS_VERSION: {info['version']}")
+    
     return info
 
 if __name__ == "__main__":
